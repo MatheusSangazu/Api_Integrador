0214fc5a5dd32bafd678ad8c4b62900d228676fd
Tteu <129115951+TteuLanchess@users.noreply.github.com>
2024-06-20 10:23:42 -0300

    Comentada a rota de subscribers

--- src/routes/botRoutes.js
+++ src/routes/botRoutes.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const { fetchSubscribers } = require('../controllers/botController');
 
 // Rota para obter dados dos inscritos
-router.get('/subscribers', fetchSubscribers);
+//router.get('/subscribers', fetchSubscribers);
 
 module.exports = router;
